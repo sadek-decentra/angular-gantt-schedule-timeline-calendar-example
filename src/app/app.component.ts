9830d1a82d1162fcefca1f3421e76e3cd023e205
sadek-decentra <sadek@decentra-tech.com>
2021-05-27 17:01:11 +0300

    ngx timeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,22 @@ import {
   ElementRef,
   OnInit,
   ViewEncapsulation,
-} from '@angular/core';
-import GSTC, { Config, GSTCResult } from 'gantt-schedule-timeline-calendar';
-import { Plugin as TimelinePointer } from 'gantt-schedule-timeline-calendar/dist/plugins/timeline-pointer.esm.min.js';
-import { Plugin as Selection } from 'gantt-schedule-timeline-calendar/dist/plugins/selection.esm.min.js';
+} from "@angular/core";
+import GSTC, { Config, GSTCResult } from "gantt-schedule-timeline-calendar";
+import { Plugin as TimelinePointer } from "gantt-schedule-timeline-calendar/dist/plugins/timeline-pointer.esm.min.js";
+import { Plugin as Selection } from "gantt-schedule-timeline-calendar/dist/plugins/selection.esm.min.js";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
+  selector: "app-root",
+  templateUrl: "app.component.html",
   styleUrls: [
-    './app.component.css',
-    '../../node_modules/gantt-schedule-timeline-calendar/dist/style.css',
+    "./app.component.css",
+    "../../node_modules/gantt-schedule-timeline-calendar/dist/style.css",
   ],
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent implements OnInit {
-  @ViewChild('gstcElement', { static: true }) gstcElement: ElementRef;
+  @ViewChild("gstcElement", { static: true }) gstcElement: ElementRef;
   gstc: GSTCResult;
 
   generateConfig(): Config {
@@ -32,7 +32,7 @@ export class AppComponent implements OnInit {
       const id = GSTC.api.GSTCID(i.toString());
       rows[id] = {
         id,
-        label: 'Room ' + i,
+        label: "Room " + i,
         parentId: withParent ? GSTC.api.GSTCID((i - 1).toString()) : undefined,
         expanded: false,
       };
@@ -40,17 +40,17 @@ export class AppComponent implements OnInit {
 
     // GENERATE SOME ROW -> ITEMS
 
-    let start = GSTC.api.date().startOf('day').subtract(30, 'day');
+    let start = GSTC.api.date().startOf("day").subtract(30, "day");
     const items = {};
     for (let i = 0; i < iterations; i++) {
       const id = GSTC.api.GSTCID(i.toString());
-      start = start.add(1, 'day');
+      start = start.add(1, "day");
       items[id] = {
         id,
-        label: 'User id ' + i,
+        label: "User id " + i,
         time: {
           start: start.valueOf(),
-          end: start.add(1, 'day').valueOf(),
+          end: start.add(1, "day").valueOf(),
         },
         rowId: id,
       };
@@ -64,15 +64,15 @@ export class AppComponent implements OnInit {
         inRealTime: true,
       },
       data: {
-        [GSTC.api.GSTCID('label')]: {
-          id: GSTC.api.GSTCID('label'),
-          data: 'label',
+        [GSTC.api.GSTCID("label")]: {
+          id: GSTC.api.GSTCID("label"),
+          data: "label",
           expander: true,
           isHtml: true,
           width: 230,
           minWidth: 100,
           header: {
-            content: 'Room',
+            content: "Room",
           },
         },
       },
@@ -80,7 +80,7 @@ export class AppComponent implements OnInit {
 
     return {
       licenseKey:
-        '====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====',
+        "====BEGIN LICENSE KEY====\nXOfH/lnVASM6et4Co473t9jPIvhmQ/l0X3Ewog30VudX6GVkOB0n3oDx42NtADJ8HjYrhfXKSNu5EMRb5KzCLvMt/pu7xugjbvpyI1glE7Ha6E5VZwRpb4AC8T1KBF67FKAgaI7YFeOtPFROSCKrW5la38jbE5fo+q2N6wAfEti8la2ie6/7U2V+SdJPqkm/mLY/JBHdvDHoUduwe4zgqBUYLTNUgX6aKdlhpZPuHfj2SMeB/tcTJfH48rN1mgGkNkAT9ovROwI7ReLrdlHrHmJ1UwZZnAfxAC3ftIjgTEHsd/f+JrjW6t+kL6Ef1tT1eQ2DPFLJlhluTD91AsZMUg==||U2FsdGVkX1/SWWqU9YmxtM0T6Nm5mClKwqTaoF9wgZd9rNw2xs4hnY8Ilv8DZtFyNt92xym3eB6WA605N5llLm0D68EQtU9ci1rTEDopZ1ODzcqtTVSoFEloNPFSfW6LTIC9+2LSVBeeHXoLEQiLYHWihHu10Xll3KsH9iBObDACDm1PT7IV4uWvNpNeuKJc\npY3C5SG+3sHRX1aeMnHlKLhaIsOdw2IexjvMqocVpfRpX4wnsabNA0VJ3k95zUPS3vTtSegeDhwbl6j+/FZcGk9i+gAy6LuetlKuARjPYn2LH5Be3Ah+ggSBPlxf3JW9rtWNdUoFByHTcFlhzlU9HnpnBUrgcVMhCQ7SAjN9h2NMGmCr10Rn4OE0WtelNqYVig7KmENaPvFT+k2I0cYZ4KWwxxsQNKbjEAxJxrzK4HkaczCvyQbzj4Ppxx/0q+Cns44OeyWcwYD/vSaJm4Kptwpr+L4y5BoSO/WeqhSUQQ85nvOhtE0pSH/ZXYo3pqjPdQRfNm6NFeBl2lwTmZUEuw==\n====END LICENSE KEY====",
       list: {
         rows,
         columns,
@@ -101,9 +101,9 @@ export class AppComponent implements OnInit {
 
   updateFirstItem(): void {
     this.gstc.state.update(
-      `config.chart.items.${GSTC.api.GSTCID('0')}`,
+      `config.chart.items.${GSTC.api.GSTCID("0")}`,
       (item) => {
-        item.label = 'Dynamically updated!';
+        item.label = "Dynamically updated!";
         return item;
       }
     );
@@ -111,9 +111,9 @@ export class AppComponent implements OnInit {
 
   updateFirstRow(): void {
     this.gstc.state.update(
-      `config.list.rows.${GSTC.api.GSTCID('0')}`,
+      `config.list.rows.${GSTC.api.GSTCID("0")}`,
       (row) => {
-        row.label = 'Dynamically updated!';
+        row.label = "Dynamically updated!";
         return row;
       }
     );
